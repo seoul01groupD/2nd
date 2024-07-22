67e68754ab85c93857393b33d3aea1fa9af47d9b
junyoung069 <jylee0619@naver.com>
2024-07-22 15:15:55 +0900

    start

--- gyoo413/1181_sort.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-word_list = []
-for i in range(n):
-    word_list.append(input())
-
-word_with_len = []
-for i in range(n):
-    word_with_len.append([word_list[i], len(word_list)])
-
