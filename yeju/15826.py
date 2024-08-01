27ec8adf170811fc41e2601544332ed2f33ca9b7
yejushin2000 <yejushin2000@naver.com>
2024-08-01 09:21:15 +0900

    a

--- yeju/15826.py
+++ yeju/15826.py
@@ -1,13 +1,14 @@
 # 15829
-# 50점
 
 L = int(input())
 word = input()
 H = 0
 
 for i in range(L):
+    # 소문자
     if 97<= ord(word[i]) <= 122:
         H += (ord(word[i])-96) * (31 ** i)
-    else: 
+    # 대문자
+    else:
         H += (ord(word[i])-64) * (31 ** i)
-print(H)
+print(H % 1234567891)
