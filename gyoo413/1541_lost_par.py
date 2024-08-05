d74c49d2b194798d400bbf978df94f52d4ca225e
Da-413 <gyoo413@naver.com>
2024-08-06 01:28:23 +0900

    gyoo413

--- gyoo413/1541_lost_par.py
+++ gyoo413/1541_lost_par.py
@@ -13,4 +13,20 @@ for i in range(len(equation)):
         lst.append(int(temp))
         temp = ''
 
-print(lst)
+next_lst = []
+temp = lst[0]
+for i in range(len(lst)):
+    
+    if lst[i] == '+':
+        temp += lst[i + 1]
+    elif lst[i] == '-':
+        next_lst.append(temp)
+        temp = lst[i + 1]
+    if i == len(lst) - 1:
+        next_lst.append(temp)
+
+ans = next_lst[0]
+for i in range(1, len(next_lst)):
+    ans -= next_lst[i]
+
+print(ans)
