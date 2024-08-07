3369ef1b5db6d70caad77040059e195cf36041fa
Da-413 <gyoo413@naver.com>
2024-08-08 00:41:42 +0900

    gn...

--- gyoo413/1931_meeting.py
+++ gyoo413/1931_meeting.py
@@ -1,7 +1,36 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 lst = []
 for i in range(n):
     lst.append(list(map(int, input().split())))
 
 lst = sorted(lst, key=lambda lst: (lst[0], lst[1]))
-print(lst)
+
+meeting = []
+small = lst[0][1]
+duration = lst[0][1] - lst[0][0]
+select = lst[0]
+
+for idx in range(1, len(lst)):
+
+    if (lst[idx][0] < small) and (lst[idx][1] - lst[idx][0] < duration):
+        select = lst[idx]
+        duration = lst[idx][1] - lst[idx][0]
+        small = lst[idx][1]
+
+    elif lst[idx][0] >= small:
+        meeting.append(select)
+        select = lst[idx]
+        duration = select[1] -select[0]
+        small = select[1]
+
+    if idx == len(lst) - 1:
+        meeting.append(select)
+    print(select)
+print(meeting)
+
+
+print(len(meeting))
+
