95d54d57fcc6b81ddf25bf3465160c1c24f2e3b3
Da-413 <gyoo413@naver.com>
2024-08-09 01:15:00 +0900

    11724

--- gyoo413/11724_edges.py
+++ gyoo413/11724_edges.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 v, e = map(int, input().split())
 lst = []
 for i in range(e):
@@ -7,32 +10,30 @@ for i in range(e):
     v1, v2 = lst[2*i], lst[2*i + 1]
     adjL[v1].append(v2)
     adjL[v2].append(v1)
-
+visited = [0] * (v + 1)
 
 def DFS(s, n):
-    visited = [0] * (n + 1)
     stack = []
     v = s
-    cnt = 0
-    for i in range(s, len(visited)):
-        if visited[i] == 0:
-            v = i
-            while True:
-                for w in adjL[v]:
-                    if not visited[w]:
-                        visited[w] = 1
-                        stack.append(v)
-                        v = w
-                        break
-                else:
-                    if stack:
-                        v = stack.pop()
-                    else:
-                        break
-            cnt += 1
+    visited[v] = 1
+    
+    while True:
+        for w in adjL[v]:
+            if visited[w] == 0:
+                visited[w] = 1
+                stack.append(v)
+                v = w
+                break
+        else:
+            if stack:
+                v = stack.pop()
+            else:
+                break
 
-    return cnt
-
-
-print(DFS(1, v))
-
+cnt = 0
+for i in range(1, v + 1):
+    if visited[i] == 0:
+        DFS(i, v)
+        cnt += 1
+    
+print(cnt)
